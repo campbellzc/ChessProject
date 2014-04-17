f154d5eb90b07aebbe064e4e44d1d1838470eff8
zenonira <zenonira@s.dcsdk12.org>
2014-04-17 09:19:02 -0600

    Bishop and Castle

--- Castle.java
+++ Castle.java
@@ -1,3 +1,23 @@
 public class Castle {
 	
+	Color color;
+	Image white;
+	Image black;
+
+	public Castle(Color color) {
+		this.color = color;
+		this.white = Bishop.loadImage("rook_white");
+		this.black = Bishop.loadImage("rook_black");
+
+	}
+
+	public void draw(Graphics g, Rectangel r) {
+		if(this.color == BLACK) {
+			g.drawImage(black, r.x, r.y, r.width, r.height, null);
+		}
+		else {
+			g.drawImage(white, r.x, r.y, r.width, r.height, null);
+		}
+	}
+
 }
