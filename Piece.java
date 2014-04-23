3f3d31c4cdc5b1766b556f154833ab5ef373cadd
zenonira <zenonira@s.dcsdk12.org>
2014-04-23 14:33:24 -0600

    Piece was worked on

--- Piece.java
+++ Piece.java
@@ -10,11 +10,33 @@ import javax.imageio.ImageIO;
 public class Piece {
 
 	private Image image;
+	private String color;
+	private String type;
+	private int x;
+	private int y;
 
+	public Piece(String color, String type, int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.type = type;
+		this.image = Piece.loadImage(type + "_" + color);
+	}
 
-	// public ArrayList<Location> validMoveLocations() {
+	public static Image loadImage(String name) {
+		String path = null;
+		Image image = null;
 
-	// }
+		try {
+			path = "pictures" + File.separator + name + ".png";
+			image = ImageIO.read(new File(path));
+		} catch(IOException e) {
+			System.out.println("Could not load image at path: " + path);
+			System.exit(1);
+		}
+
+		return image;
+	}
 
 	public void Act() {
 
@@ -34,20 +56,7 @@ public class Piece {
 
 
 
-	public static Image loadImage(String name) {
-		String path = null;
-		Image image = null;
-
-		try {
-			path = "pictures" + File.separator + name + ".png";
-			image = ImageIO.read(new File(path));
-		} catch(IOException e) {
-			System.out.println("Could not load image at path: " + path);
-			System.exit(1);
-		}
-
-		return image;
-	}
+	
 
 
 }
