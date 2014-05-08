8448118e4f7278bd98c17766098ecf7f8aa6acad
Campbell <campbellzc@s.dcsdk12.org>
2014-05-08 09:05:56 -0600

    added stuff to piece

--- Bishop.java
+++ Bishop.java
@@ -1,7 +1,10 @@
 public class Bishop extends Piece {
-
-	public Bishop() {
-
+	private Color color
+	public Bishop(Color colo, int x, int y) {
+		this.color = colo;
+		this.x = x;
+		this.y = y;
+		this.icon = new ImageIcon(Peice.loadImage("Bishop", color));
 	}
 
 }
