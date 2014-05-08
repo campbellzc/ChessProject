2dae55fcf918bffd59ac728f3bae59d0d6038483
apcs <apcs@mvhs-u52810.dcsdk12.org>
2014-05-08 08:28:21 -0600

    Making dat progress

--- TableApplet.java
+++ TableApplet.java
@@ -15,53 +15,65 @@ public class TableApplet extends Applet implements ActionListener{
 	private JButton btn;
 
 	public void init() {
+		
+		setLayout(new GridLayout(8,8));
+		JButton[][] board = new JButton[8][8];
 
-		for (int i=0; i<64; i++) {
-			setLayout(new GridLayout(8,8));
-			btn = new JButton();
-			if (i>7 && i<16 || i>23 && i<32 || i>39 && i<48 || i>55 && i<64) {
+		for (int i=0; i<8; i++) {
+
+			for (int j=0; j<8; j++) {
+				btn = new JButton();
+				btn.setPreferredSize(new Dimension(40, 40));
+				board[i][j] = btn;
+
 				if (i%2 == 0) {
-					btn.setBackground(Color.BLACK);
+					if (j%2 == 0) {
+						btn.setBackground(new Color(235,199,158));
+					} else {
+						// dark color
+						btn.setBackground(new Color(92,51,23));
+					}
 				} else {
-					btn.setBackground(Color.WHITE);
+					if (j%2 == 0) {
+						// dark color
+						btn.setBackground(new Color(92,51,23));
+					} else {
+						btn.setBackground(new Color(235,199,158));
+					}
 				}
-			} else {
-				if (i%2 == 0) {
-					btn.setBackground(Color.WHITE);
-				} else {
-					btn.setBackground(Color.BLACK);
-				}
+
+				btn.setBorderPainted(false);
+				btn.setOpaque(true);
+				btn.addActionListener(this);
+				this.add(btn);
+
+				// String path = null;
+				// Image image = null;
+
+				// if (i<2) {
+				// 	try {
+				// 	    path = "pictures" + File.separator + "pawn_black.png";
+				// 	    File file = new File(path);
+				// 	    Image img = ImageIO.read(new File(path));
+				// 	    btn.setIcon(new ImageIcon(img));
+				// 	  } catch (IOException ex) {
+				// 	  	System.out.println("Could not load image at path: " + path);
+				// 	  	System.exit(1);
+				// 	  }
+				// } else if (i>5) {
+				// 	try {
+				// 	    path = "pictures" + File.separator + "pawn_white.png";
+				// 	    File file = new File(path);
+				// 	    Image img = ImageIO.read(new File(path));
+				// 	    btn.setIcon(new ImageIcon(img));
+				// 	  } catch (IOException ex) {
+				// 	  	System.out.println("Could not load image at path: " + path);
+				// 	  	System.exit(1);
+				// 	  }
+				// }
+
 			}
 
-			String path = null;
-			Image image = null;
-
-			if (i>7 && i<16) {
-				try {
-				    path = "pictures" + File.separator + "pawn_black.png";
-				    File file = new File(path);
-				    Image img = ImageIO.read(new File(path));
-				    btn.setIcon(new ImageIcon(img));
-				  } catch (IOException ex) {
-				  	System.out.println("Could not load image at path: " + path);
-				  	System.exit(1);
-				  }
-			} else if (i>47 && i<56) {
-				try {
-				    path = "pictures" + File.separator + "pawn_white.png";
-				    File file = new File(path);
-				    Image img = ImageIO.read(new File(path));
-				    btn.setIcon(new ImageIcon(img));
-				  } catch (IOException ex) {
-				  	System.out.println("Could not load image at path: " + path);
-				  	System.exit(1);
-				  }
-			}
-			btn.setContentAreaFilled(false);
-			btn.setForeground(Color.BLACK);
-			btn.setOpaque(true);
-			btn.addActionListener(this);
-			this.add(btn);
 		}
 
 	}
@@ -69,12 +81,6 @@ public class TableApplet extends Applet implements ActionListener{
 	public void paint(Graphics g) {
 		super.paint(g);
 	}
-
-	// Peice test = new Peice();
-	// public void paint(Graphics g) {
-	// 	super.paint(g);
-	// 	test.draw(bishop_black);
-	// }
 	
 	public void actionPerformed(ActionEvent e) {
 
